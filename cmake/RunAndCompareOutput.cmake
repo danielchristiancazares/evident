7c3cf9aaec627a576c859a570a5733b9ea6c26ed
Daniel Cazares <daniel.cazares@gmail.com>
2026-06-27 06:51:59 -0700

    Normalize bootstrap provenance comparisons

--- cmake/RunAndCompareOutput.cmake
+++ cmake/RunAndCompareOutput.cmake
@@ -37,6 +37,29 @@ if(NOT command_result EQUAL 0)
         "compiler command failed with exit code ${command_result}\nstdout:\n${command_stdout}\nstderr:\n${command_stderr}")
 endif()
 
+if(DEFINED NORMALIZE_BOOTSTRAP_SOURCE_COMMIT)
+    if(NOT DEFINED SOURCE_DIR)
+        get_filename_component(SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/.." ABSOLUTE)
+    endif()
+
+    execute_process(
+        COMMAND git rev-parse HEAD
+        WORKING_DIRECTORY "${SOURCE_DIR}"
+        RESULT_VARIABLE git_result
+        OUTPUT_VARIABLE source_commit
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    if(NOT git_result EQUAL 0 OR source_commit STREQUAL "")
+        message(FATAL_ERROR "failed to determine bootstrap source commit for normalization")
+    endif()
+    string(LENGTH "${source_commit}" source_commit_length)
+    if(NOT source_commit_length EQUAL 40)
+        message(FATAL_ERROR "unexpected bootstrap source commit '${source_commit}'")
+    endif()
+
+    string(REGEX REPLACE "\"${source_commit}\"" "\"source commit recorded in release evidence\"" command_stdout "${command_stdout}")
+endif()
+
 file(READ "${EXPECTED_PATH}" expected_output)
 if(NOT command_stdout STREQUAL expected_output)
     message(FATAL_ERROR
